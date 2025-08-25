ba4ba6d543d10adb8688264f8ac43de887fb8980
Kirill Belousov <cyrmax@internet.ru>
2025-08-25 21:19:09 +0300

    Another workflow fix

--- scripts/build-release32.bat
+++ scripts/build-release32.bat
@@ -1,1 +1,1 @@
-cmake --preset windows-msvc-x86-release && cmake --build --preset release-x86
+cmake --preset windows-msvc-x86-release -DSIM_VERSION="%GITHUB_REF_NAME%" -DSIM_BUILD_NUMBER="%GITHUB_RUN_NUMBER%" && cmake --build --preset release-x86
